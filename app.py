aaf3a29b00be96c5abb154401378ad36ff595b7e
cadee-exe <cadeez@nycstudents.net>
2025-09-16 06:24:34 -0700

    app.py

---
+++ app.py
@@ -0,0 +1,25 @@
+x = 3
+y = float(3)
+print(x,y)
+values = [1,2.23,5,7,2,30,15]
+print(values)
+for i in values:
+    print(i)
+    
+print(values[0])
+print(values[6])
+
+x = "this is a thing"
+y= x.split( )
+z = y[0]
+print(y)
+print(z)
+
+day_of_week = input("what day is it? ")
+if day_of_week == "Friday":
+    print("correct")
+else:
+    print("incorrect")
+
+x = "test"
+print(f"hello {x}")
